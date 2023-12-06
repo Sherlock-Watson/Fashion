381240f6bdbba1ed8b57a34f924e2625f141bc08
Xiang Xinye <aenea@Aeneas-macbook.local>
2023-12-06 14:19:02 +0800

    generate label file🥳

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import utils
-from PIL import Image
 
 # Data columns (total 6 columns):
 #  #   Column         Non-Null Count   Dtype
@@ -17,29 +16,22 @@ output_file = "output"
 os.makedirs(output_file, exist_ok=True)
 path = 'imaterialist-fashion-2020-fgvc7'
 label_folder = os.path.join(path, "train_labels")
-image_path = os.path.join(path, "train")
+os.makedirs(label_folder, exist_ok=True)
+train_image_folder = os.path.join(path, "train")
 csv_path = os.path.join(path, "train.csv")
-image_list = os.listdir(image_path)
+
+# image_list = os.listdir(train_image_folder)
 # print(image_list)
-df = pd.read_csv(csv_path)
+# df = pd.read_csv(csv_path)
+# first_line = df.iloc[0]
+# img_id = first_line['ImageId']
+# mask_rle = first_line['EncodedPixels']
+# height = first_line['Height']
+# width = first_line['Width']
+# class_id = first_line['ClassId']
+# attribute_ids = first_line['AttributesIds']
+# img_shape = (width, height)
+# print(f"image_id: {img_id}, height: {height}, width: {width}, class id: {class_id}, attribute ids: {attribute_ids}")
+# label_path = os.path.join(label_folder, f"{img_id}.txt")
+# utils.save_label(label_path, mask_rle, img_shape, class_id)
 
-
-
-
-first_line = df.iloc[0]
-img_id = first_line['ImageId']
-mask_rle = first_line['EncodedPixels']
-height = first_line['Height']
-width = first_line['Width']
-class_id = first_line['ClassId']
-attribute_ids = first_line['AttributesIds']
-img_shape = (width, height)
-# image_file = Image.open(os.path.join(image_path, f"{img_id}.jpg"))
-# print(f"image_shape: {image_file.size}")
-print(f"image_id: {img_id}, height: {height}, width: {width}, class id: {class_id}, attribute ids: {type(attribute_ids)}")
-decoded_mask = utils.rle_decode(mask_rle, img_shape)
-utils.test_segmentation_mask_on_original_file(os.path.join(image_path, f"{img_id}.jpg"), utils.get_segmentation_mask(decoded_mask))
-# mask_image = Image.fromarray(decoded_mask * 255)
-# mask_image.save(os.path.join(output_file, f'{img_id}_{class_id}.png'))
-# print(decoded_mask)
-# print(df.info())
--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ from PIL import Image
 # col_ = 'AttributesIds'
 
 
-def rle_decode(param_mask_rle, shape):
+def rle_decode(mask_rle, shape):
     """
     解码Run-Length Encoded格式的掩码。
 
@@ -21,7 +21,7 @@ def rle_decode(param_mask_rle, shape):
     Returns:
     - mask (numpy.ndarray): 解码后的二进制掩码数组
     """
-    s = param_mask_rle.split()
+    s = mask_rle.split()
     starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
     starts -= 1
     ends = starts + lengths
@@ -33,24 +33,29 @@ def rle_decode(param_mask_rle, shape):
     return img.reshape(shape)
 
 
-# test methods
-def get_segmentation_mask(binary_mask):
+def get_segmentation_mask(mask_rle, shape):
+    binary_mask = rle_decode(mask_rle, shape)
     result = []
-    row_num, col_num = binary_mask.shape
+    row_num, col_num = shape
     print(f"row_num: {row_num}, col_num: {col_num}")
     for i in range(1, row_num):
         for j in range(1, col_num):
             i_ratio = float(i) / row_num
             j_ratio = float(j) / col_num
-            if binary_mask[i][j] != binary_mask[i - 1][j]:
-                result.append(i_ratio)
-                result.append(j_ratio)
-            elif binary_mask[i][j] != binary_mask[i][j - 1]:
+            if binary_mask[i][j] != binary_mask[i - 1][j] or binary_mask[i][j] != binary_mask[i][j - 1]:
                 result.append(i_ratio)
                 result.append(j_ratio)
     return result
 
 
+def save_label(file_path, mask_rle, shape, class_id):
+    result_list = get_segmentation_mask(mask_rle, shape)
+    content = f"{class_id} {' '.join([str(f) for f in result_list])}\n"
+    with open(file_path, mode='a') as file:
+        file.write(content)
+
+
+# test methods
 def test_segmentation_mask(binary_mask, result_list):
     original_image = Image.fromarray(binary_mask * 255, "L").convert('RGB')
     print(original_image.size)
@@ -69,3 +74,17 @@ def test_segmentation_mask_on_original_file(file_path, result_list):
     for x, y in zip(x_list, y_list):
         pixels[x, y] = (255, 0, 0)
     original_image.save("output/image.png")
+
+
+def test_label_file(file_path, shape):
+    with open(file_path, mode="r") as file:
+        content = file.readline()
+    value_list = content.split()
+    x_list, y_list = [np.asarray(x, dtype=float) for x in (value_list[1:][::2], value_list[2:][::2])]
+    img = np.ones(shape, dtype=np.uint8)
+    for x, y in zip(x_list, y_list):
+        x_index = int(x * shape[1])
+        y_index = int(y * shape[0])
+        img[y_index][x_index] = 0
+    image = Image.fromarray(img * 255)
+    image.save("output/mask_boundary_line.png")
